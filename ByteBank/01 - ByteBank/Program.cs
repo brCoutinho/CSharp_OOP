48cbf5f39bdb1387f5d899e29db05a2b1480f0ca
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-02-07 14:08:52 -0300

    Início do ByteBank

---
+++ ByteBank/01 - ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoThiago = new ContaCorrente();
+
+            contaDoThiago.titular = "Thiago";
+            contaDoThiago.agencia = 1234;
+            contaDoThiago.numero = 123456;
+            contaDoThiago.saldo = 100;
+
+            Console.WriteLine(contaDoThiago.titular);
+            Console.WriteLine(contaDoThiago.agencia);
+            Console.WriteLine(contaDoThiago.numero);
+            Console.WriteLine(contaDoThiago.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
