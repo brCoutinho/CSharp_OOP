32db6df3980b24d8adad12b4a855ab0caecb244c
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-02-10 22:12:58 -0300

    Primeira Função get/set

---
+++ ByteBank/04 - ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaMaria = new ContaCorrente();
+
+            contaDaMaria.titular = "Maria";
+
+            Console.WriteLine(contaDaMaria.saldo);
+            contaDaMaria.Sacar(50);
+            Console.WriteLine(contaDaMaria.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
