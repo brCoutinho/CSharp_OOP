1721ce47c11949bbd713a6a8831a9e3ef4824ba0
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-03-08 10:55:26 -0300

    ByteBank

--- ByteBank/04 - ByteBank/Program.cs
+++ ByteBank/04 - ByteBank/Program.cs
@@ -18,6 +18,20 @@ namespace _04___ByteBank
             contaDaMaria.Sacar(50);
             Console.WriteLine(contaDaMaria.saldo);
 
+            contaDaMaria.Depositar(500);
+
+            Console.WriteLine(contaDaMaria.saldo);
+
+            ContaCorrente contaDaDuda = new ContaCorrente();
+
+            contaDaDuda.titular = "Maria Eduarda";
+
+            contaDaMaria.Transferir(100, contaDaDuda);
+
+            Console.WriteLine("Saldo da Conta da Maria: " + contaDaMaria.saldo);
+            Console.WriteLine("Saldo da Conta da Duda: " + contaDaDuda.saldo);
+
+
             Console.ReadLine();
         }
     }
---
+++ ByteBank/05 - ByteBank/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente gabriela = new Cliente();
+
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissao = "Engenheira";
+            //gabriela.cpf = "444.444.444-44";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = new Cliente();
+            conta.titular.nome = "Gabriela Costa";
+            conta.titular.cpf = "8888888899";
+            conta.titular.profissao = "Advogada";
+
+
+            //conta.titular = gabriela;
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 65345;
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+            Console.WriteLine(conta.titular.cpf);
+            Console.WriteLine(conta.titular.profissao);
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
