77cccec657f9f534661a6c162b2e492b2310e1bb
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-03-08 22:06:38 -0300

    Introdução ao Get e Set

---
+++ ByteBank/06 - ByteBank/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06___ByteBank
+{
+    public class Cliente
+    {
+        //prop tab + tab
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Profissao { get; set; }
+    }
+}
---
+++ ByteBank/06 - ByteBank/ContaCorrente.cs
@@ -0,0 +1,65 @@
+﻿
+
+
+namespace _06___ByteBank
+{
+    public class ContaCorrente
+    {
+        private Cliente Titular { get; set; }
+        private int Agencia { get; set; }
+        private int Numero { get; set; }
+        private double saldo = 100;
+
+
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                this.saldo = value;
+            }
+        }
+
+       
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
+
