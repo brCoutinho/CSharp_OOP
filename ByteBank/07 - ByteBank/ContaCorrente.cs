5efc18a63a1626ac39a7d701ecbc320b19d1490e
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-03-10 21:50:01 -0300

    Construtores e membros estáticos

---
+++ ByteBank/07 - ByteBank/ContaCorrente.cs
@@ -0,0 +1,74 @@
+﻿
+
+
+namespace _07___ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        public double saldo = 100;
+        public static int TotalDeContasCriadas { get; private set; }
+
+
+        //construtor
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                this.saldo = value;
+            }
+        }
+
+       
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
+
---
+++ ByteBank/07 - ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(1234,3561818);
+
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+
+            ContaCorrente contaDaNathalia = new ContaCorrente(8589, 3547828);
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+             
+
+            Console.ReadLine();
+
+        }
+    }
+}
