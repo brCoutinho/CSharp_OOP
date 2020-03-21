553a441ad4c7b1bc486fbc2a7db885bcb385b1fe
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-03-21 10:54:05 -0300

    Entendendo Herança e Sobrecarga

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -15,14 +15,29 @@ namespace ByteBank
             //  *Quando a diretiva não está nos using do projeto >>
             //      >>Cursor em cima da palavra > CTRL + . > Verificar as opções > Enter
 
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+
             Funcionario thiago = new Funcionario();
 
             thiago.Nome = "Thiago";
             thiago.CPF = "444.444.448-89";
             thiago.Salario = 3000;
 
-            Console.WriteLine(thiago.Nome);
-            Console.WriteLine(thiago.GetBonificacao());
+            gerenciador.Registrar(thiago);
+
+            Diretor roberta = new Diretor();
+
+            roberta.Nome = "Roberta";
+            roberta.CPF = "333333333";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonificação: "+ gerenciador.GetTotalBonificacao());
 
             Console.ReadLine();
         }
